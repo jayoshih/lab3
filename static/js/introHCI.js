cbe23e39472cfeef1defeb3edfe45b9e661fb30c
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-01-21 12:22:47 -0800

    Final Submission

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,40 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		console.log("Project clicked");
+		$('#testjs').text("Javascript is connected");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".thumbnail").click(projectClick);
+	$(".project").click(toggleProject)
+    $("#submitBtn").click(submitChanges); 
+} 
+
+function submitChanges(e) {
+   var pid = $('#project').val();
+   $(pid).animate({width: $('#width').val()});
+   $(pid + " .project-description").text($('#description').val());
+}
+
+function projectClick(e){
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<p class='project-description'>Description of the project.</p>");
+    } else {
+       description.remove();
+	}
+}
+
+function toggleProject(e){
+	if(e.target.tagName != "img"){
+		e.preventDefault();
+		$(this).find(".project-description").toggle();
+		$(this).find("img").toggle();
+	}
 }
